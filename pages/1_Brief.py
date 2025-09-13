7b500636feeb8a1dd4cb276541b9ede7ad6562fa
zekobiceps <zakariametinn@gmail.com>
2025-09-13 16:53:30 +0000

    feat: Refactor IA et bibliothèque 1

--- pages/1_Brief.py
+++ pages/1_Brief.py
@@ -610,7 +610,7 @@ with tabs[0]:
         with col5:
             st.text_input("Nom de l'affectation", key="affectation_nom")
         with col6:
-            st.date_input("Date du Brief *", key="date_brief", value=datetime.today().date())
+            st.date_input("Date du Brief *", key="date_brief", value=st.session_state.get("date_brief", datetime.today().date()))
         
         # --- SAUVEGARDE - Bouton étendu
         if st.button("💾 Sauvegarder", type="primary", use_container_width=True, key="save_gestion"):
